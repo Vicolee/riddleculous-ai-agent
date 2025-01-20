3abad62adf9c8157f9ef9557d0794067a5f87d24
Vico Lee <vicoleezy@gmail.com>
2025-01-20 13:55:21 -0500

    moved package

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -99,7 +99,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import yargs from "yargs";
 
-import { mainCharacter } from "../mainCharacter";
+import { mainCharacter } from "./mainCharacter";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
